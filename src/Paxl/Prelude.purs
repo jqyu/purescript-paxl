acec6a4fc6ce6f37945f4a00a2b5a12b748bcf5a
James Yu <james@jamesy.us>
2018-03-18 20:08:04 -0700

    export (>>) and (<<) for fun

--- src/Paxl/Prelude.purs
+++ src/Paxl/Prelude.purs
@@ -5,6 +5,8 @@ module Paxl.Prelude
   , ApplyRowEffect
   , (<|)
   , (|>)
+  , (<<)
+  , (>>)
   , discard
   , module ReExports
   ) where
@@ -14,7 +16,8 @@ import Control.Monad.Eff (kind Effect)
 import Control.Semigroupoid (compose, composeFlipped)
 import Data.Function (apply, applyFlipped)
 import Data.Unit (Unit, unit)
-import Prelude hiding (($), (#), discard) as ReExports
+import Prelude (class Applicative, class Apply, class Bind, class BooleanAlgebra, class Bounded, class Category, class CommutativeRing, class Discard, class DivisionRing, class Eq, class EuclideanRing, class Field, class Functor, class HeytingAlgebra, class Monad, class Ord, class Ring, class Semigroup, class Semigroupoid, class Semiring, class Show, type (~>), Ordering(..), Unit, Void, absurd, add, ap, append, apply, between, bind, bottom, clamp, compare, comparing, compose, conj, const, degree, disj, div, eq, flap, flip, gcd, id, ifM, join, lcm, liftA1, liftM1, map, max, min, mod, mul, negate, not, notEq, one, otherwise, pure, recip, show, sub, top, unit, unless, unlessM, void, when, whenM, zero, ($>), (&&), (*), (*>), (+), (-), (/), (/=), (<), (<#>), (<$), (<$>), (<*), (<*>), (<<<), (<=), (<=<), (<>), (<@>), (=<<), (==), (>), (>=), (>=>), (>>=), (>>>), (||)) as ReExports
+
 
 
 type ApplyRowType (f ∷ # Type → # Type) r = f r
